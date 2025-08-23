80fc110bd6187deecb93cefb178b3ee14be9b88f
hyeonklab <hyeonk.lab@gmail.com>
2025-08-23 17:16:03 +0900

    Update Program.cs

--- Batch/Program.cs
+++ Batch/Program.cs
@@ -5,10 +5,10 @@ Console.WriteLine("Batch processing started...");
 Console.WriteLine();
 Console.WriteLine("RunBatch 1");
 BatchLib batchLib = new BatchLib();
-batchLib.RunBatch(Enumerable.Range(1, 50).ToList(), 10, 1, 100); // do run async with batch size 10 from 1 to 50 samples by seconds and milliseconds
+batchLib.RunBatch(Enumerable.Range(1, 50).ToList(), 10, 1, 100); // do run with batch size 10 from 1 to 50 samples by seconds and milliseconds
 
 Console.WriteLine();
 Console.WriteLine("RunBatch 2");
-batchLib.RunBatch(Enumerable.Range(1, 50).ToList(), 10, TimeSpan.FromSeconds(1), TimeSpan.FromMilliseconds(100)); // do run async with batch size 10 from 1 to 50 samples by timespan
+batchLib.RunBatch(Enumerable.Range(1, 50).ToList(), 10, TimeSpan.FromSeconds(1), TimeSpan.FromMilliseconds(100)); // do run with batch size 10 from 1 to 50 samples by timespan
 
 Console.WriteLine("Batch processing completed.");
