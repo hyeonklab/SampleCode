c0b07e4a015a3db8877abe193ba7a48a3df365ea
hyeonklab <hyeonk.lab@gmail.com>
2025-08-23 19:02:48 +0900

    Update Program.cs

--- Batch/Program.cs
+++ Batch/Program.cs
@@ -2,14 +2,13 @@
 
 Console.WriteLine("Batch processing started...");
 
-int totalItems = 50;
 Console.WriteLine();
-Console.WriteLine($"RunBatch 1: total {totalItems}");
-var batchLib = new BatchLib<int>([.. Enumerable.Range(1, totalItems)]);
-batchLib.Run(20, d => Console.WriteLine($"    Action processing: {d}"), 100); // do run with batch size 10 from 1 to 50 samples by milliseconds
+Console.WriteLine($"RunBatch 1: total {50}");
+var batchLib = new BatchLib<int>([.. Enumerable.Range(1, 50)]);
+batchLib.Run(20, d => Console.WriteLine($"    Action processing: {d}"), 100); // do run with batch size 20 from 1 to 50 samples by milliseconds
 
 Console.WriteLine();
-Console.WriteLine($"RunBatch 2: total {totalItems}");
+Console.WriteLine($"RunBatch 2: total {30}");
 batchLib = new BatchLib<int>([.. Enumerable.Range(1, 30)]);
 batchLib.Run(10, d => Console.WriteLine($"    Action processing: {d}"), 100); // do run with batch size 10 from 1 to 30 samples by milliseconds
 
